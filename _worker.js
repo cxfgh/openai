0de91b1f27c964058584de442e62292b7e6ff85a
cxfgh <34827020+cxfgh@users.noreply.github.com>
2023-04-05 18:50:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,9 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    //url.host = "api.openai.com";
-    url.host = "modheader.com";
+    url.host = "api.openai.com";
+    //需要反向代理的地址
+    //url.host = "modheader.com";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
